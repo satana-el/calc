a97bf1598a81aa1d9ade1bcc2941a1a71577a4ae
helel <liladharbhandarkar@proton.me>
2023-10-31 21:43:07 +0530

    Imporved spagetti code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ root = tk.Tk()
 root.title("Calculator")
 root.geometry("400x400")
 
-# Disaple resizing
+# Disable resizing
 root.minsize(400, 500)
 root.maxsize(400, 500)
 
@@ -15,44 +15,26 @@ equation_box = tk.Entry(root, textvariable=equation_var, font=("Helvetica", 25),
 equation_box.grid(row=0, column=0, columnspan=4, rowspan=2, sticky="nsew")
 
 # Buttons
-# Operators
-plus = tk.Button(root, text="+", font=("Helvetica", 16), width = 5, height = 3)
-minus = tk.Button(root, text="-", font=("Helvetica", 16), width = 5, height = 3)
-multi = tk.Button(root, text="×", font=("Helvetica", 16), width = 5, height = 3)
-div = tk.Button(root, text="÷", font=("Helvetica", 16), width = 5, height = 3)
-equals = tk.Button(root, text="=", font=("Helvetica", 16), width = 5, height = 3)
-dot = tk.Button(root, text=".", font=("Helvetica", 16), width = 5, height = 3)
+buttons = [
+    "7", "8", "9", "+",
+    "4", "5", "6", "-",
+    "1", "2", "3", "×",
+    "0", ".", "=", "÷"
+]
 
-# Numbers
-seven = tk.Button(root, text="7", font=("Helvetica", 16), width = 5, height = 3)
-eight = tk.Button(root, text="8", font=("Helvetica", 16), width = 5, height = 3)
-nine = tk.Button(root, text="9", font=("Helvetica", 16), width = 5, height = 3)
-four = tk.Button(root, text="4", font=("Helvetica", 16), width = 5, height = 3)
-five = tk.Button(root, text="5", font=("Helvetica", 16), width = 5, height = 3)
-six= tk.Button(root, text="6", font=("Helvetica", 16), width = 5, height = 3)
-one = tk.Button(root, text="1", font=("Helvetica", 16), width = 5, height = 3)
-two= tk.Button(root, text="2", font=("Helvetica", 16), width = 5, height = 3)
-three = tk.Button(root, text="3", font=("Helvetica", 16), width = 5, height = 3)
-zero = tk.Button(root, text="0", font=("Helvetica", 16), width = 5, height = 3)
+# Initailize row values, so that they don't collide with equation_box
+row_val = 2
+col_val = 0
 
-# Put the buttons on the grid.
-plus.grid(row=2, column=3, padx=5, pady=3)
-minus.grid(row=3, column=3, padx=5, pady=3)
-multi.grid(row=4, column=3, padx=5, pady=3)
-div.grid(row=5, column=3, padx=5, pady=3)
-equals.grid(row=5, column=2, padx=5, pady=3)
-dot.grid(row=5, column=1, padx=5, pady=3)
+# Buttons
+for button_value in buttons:
+    button = tk.Button(root, text=button_value, font=("Helvetica", 16), width = 5, height = 3)
+    button.grid(row = row_val, column=col_val, padx = 5, pady = 3)
 
-seven.grid(row=2, column=0, padx=5, pady=3)
-eight.grid(row=2, column=1, padx=5, pady=3)
-nine.grid(row=2, column=2, padx=5, pady=3)
-four.grid(row=3, column=0, padx=5, pady=3)
-five.grid(row=3, column=1, padx=5, pady=3)
-six.grid(row=3, column=2, padx=5, pady=3)
-one.grid(row=4, column=0, padx=5, pady=3)
-two.grid(row=4, column=1, padx=5, pady=3)
-three.grid(row=4, column=2, padx=5, pady=3)
-zero.grid(row=5, column=0, padx=5, pady=3)
+    col_val += 1
+    if col_val > 3:
+        col_val = 0
+        row_val +=1
 
 # Configure row and column weights to make the grid cells expandable
 for i in range(5):
